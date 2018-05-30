0f32301de3ed7f1020baa540b745620acd22b7e8
Vinicius Eduardo <34385449+ctzk@users.noreply.github.com>
2018-05-30 16:00:48 -0300

    Update Labcomp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,26 @@
 using namespace std;
 
 int main(){
+	int i, j, cont = 15;
+	Elemento test("^^^ ", obstaculo);
+	Pirata vrau("PRT ", pirata, 100, 70, 0);
+	Marinha poli("MRN ", marinha, 100, 60, false);
+	GrandLine cenario_jogo(cont);
+
+//	cout << cenario_jogo.getCenario()[0][0].getNome() << endl;
+
+	cenario_jogo.setCenario(vrau, 0, 0);
+	cenario_jogo.setCenario(poli, 10, 4);
+	cenario_jogo.setCenario(poli, 4, 9);
+	cenario_jogo.setCenario(test, 5, 0);
+	cenario_jogo.setCenario(test, 9, 8);
+	cenario_jogo.setCenario(test, 2, 10);
+	for(i = 0; i < cont; i++){
+		for(j = 0; j < cont; j++){
+			cout << cenario_jogo.getCenario()[i][j].getNome();
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
